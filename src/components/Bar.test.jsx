6539a10a170311fcc171c583875b4d15f84c7c63
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-09-21 12:12:59 -0400

    test: remove bar click tests & add existence tests
    Previously was testing whether `react-router` would navigate. `react-router` does plenty of testing themselves. New tests test the existence of the `Link`s.

---
+++ src/components/Bar.test.jsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Bar from "./Bar";
+import { MemoryRouter } from "react-router";
+
+function renderBar() {
+  render(
+    <MemoryRouter>
+      <Bar />
+    </MemoryRouter>,
+  );
+}
+
+describe("Bar", () => {
+  it("renders name", () => {
+    renderBar();
+    expect(screen.getByRole("heading").textContent).toMatch(
+      /we will scam you!/i,
+    );
+  });
+
+  it("has a home page link", () => {
+    renderBar();
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+  });
+
+  it("has a shop page link", () => {
+    renderBar();
+    expect(screen.getByRole("link", { name: "Shop" })).toBeInTheDocument();
+  });
+
+  it("has a cart page link", () => {
+    renderBar();
+    expect(screen.getByRole("link", { name: "Cart" })).toBeInTheDocument();
+  });
+});
